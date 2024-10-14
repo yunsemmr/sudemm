27122ef0f198eaa62488f9bd8eca7735c58f8d6d
yunsemmr <yunsemmr02@gmail.com>
2024-10-14 04:46:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function yesOption() {
+    // Yeşil butona tıklandığında yönlendirme
+    window.location.href = "affettin.html";
+}
+
+function noOption() { 
+    // Kırmızı butona tıklandığında yönlendirme
+    window.location.href = "affetmedin.html";
+}
